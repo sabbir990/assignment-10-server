2e2052e594de7fd793b202a3b7fbe3b28cdd8c69
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-06-22 08:04:45 +0100

    Added: mongodb configuration and spot's post request

--- index.js
+++ index.js
@@ -3,11 +3,38 @@ const app = express();
 const cors = require('cors');
 const port = process.env.port || 5000;
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.p2btb5w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Middlewares
 
 app.use(cors());
 app.use(express.json());
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const spotDB = client.db('spotDB').collection("spotCollection");
+
+        app.post('/spots', async(req, res) => {
+            const spot = req.body;
+            const result = await spotDB.insertOne(spot);
+            res.send(result)
+        })
+    } finally {
+        
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('My server is now running')
